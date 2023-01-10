f3918cc94a4705ffbe9a3cdc0cd463aa00d60f06
Eszenyi Gábor <eszenyi.gabor@icloud.com>
2023-01-10 23:06:46 +0100

    Resolver package added to the project.

---
+++ ArgyleLink/DependencyInjection/Resolver+Injection.swift
@@ -0,0 +1,14 @@
+//
+//  Resolver+Injection.swift
+//  ArgyleLink
+//
+//  Created by Eszenyi Gábor on 2023. 01. 10..
+//
+
+import Foundation
+import Resolver
+
+extension Resolver: ResolverRegistering {
+
+    public static func registerAllServices() {}
+}

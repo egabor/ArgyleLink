9c4b236cfde89bf401f0fe7bd62856dbec58cccc
Eszenyi Gábor <eszenyi.gabor@icloud.com>
2023-01-10 18:03:22 +0100

    Project created.

---
+++ ArgyleLink/ArgyleLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArgyleLinkApp.swift
+//  ArgyleLink
+//
+//  Created by Eszenyi Gábor on 2023. 01. 10..
+//
+
+import SwiftUI
+
+@main
+struct ArgyleLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArgyleLink/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ArgyleLink
+//
+//  Created by Eszenyi Gábor on 2023. 01. 10..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

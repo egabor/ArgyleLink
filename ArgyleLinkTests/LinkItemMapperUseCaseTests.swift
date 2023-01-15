c10e1951507d626c729fd60f5eb01985ff5fcd6c
Eszenyi Gábor <eszenyi.gabor@icloud.com>
2023-01-15 13:53:13 +0100

    Unit tests added.

--- ArgyleLinkTests/LinkItemMapperUseCaseTests.swift
+++ ArgyleLinkTests/LinkItemMapperUseCaseTests.swift
@@ -6,10 +6,13 @@
 //
 
 import XCTest
+import Resolver
 @testable import ArgyleLink
 
 // swiftlint:disable line_length
 final class LinkItemMapperUseCaseTests: XCTestCase {
+
+    @LazyInjected private var decoder: JSONDecoder
 
     let mapLinkItem: LinkItemMapperUseCase = .init()
 
@@ -30,7 +33,38 @@ final class LinkItemMapperUseCaseTests: XCTestCase {
 
     func test_linkItemMapper_shouldReturn_companyListItemViewModel() throws {
         let input = LinkItemResponse(id: "testId", name: "testName", kind: .creator, logoUrl: "https://res.cloudinary.com/argyle-media/image/upload/v1598543068/partner-logos/amazon.png")
-        let output = CompanyListItemViewModel(id: "testId", name: "testName", kind: "creator", logoUrl: "https://res.cloudinary.com/argyle-media/image/upload/v1598543068/partner-logos/amazon.png")
+        let output = CompanyListItemViewModel(id: "testId", name: "testName", kind: "creator", logoUrl: URL(string: "https://res.cloudinary.com/argyle-media/image/upload/v1598543068/partner-logos/amazon.png"))
+
+        XCTAssertEqual(mapLinkItem(input), output)
+    }
+
+    func test_linkItemMapper_whenUrlIsNil() {
+        let input = LinkItemResponse(id: "testId", name: "testName", kind: .creator, logoUrl: nil)
+        let output = CompanyListItemViewModel(id: "testId", name: "testName", kind: "creator", logoUrl: nil)
+
+        XCTAssertEqual(mapLinkItem(input), output)
+    }
+
+    func test_linkItemMapper_whenUrlIsInvalid() {
+        let input = LinkItemResponse(id: "testId", name: "testName", kind: .creator, logoUrl: "")
+        let output = CompanyListItemViewModel(id: "testId", name: "testName", kind: "creator", logoUrl: nil)
+
+        XCTAssertEqual(mapLinkItem(input), output)
+    }
+
+    func test_linkItemMapper_whenKindIsInvalid() {
+        let inputData = """
+{
+  "id": "testId",
+  "name": "testName",
+  "kind": "randomkind",
+  "logo_url": "https://res.cloudinary.com/argyle-media/image/upload/v1598543068/partner-logos/amazon.png"
+}
+""".data(using: .utf8)!
+
+        // swiftlint:disable:next force_try
+        let input = try! decoder.decode(LinkItemResponse.self, from: inputData)
+        let output = CompanyListItemViewModel(id: "testId", name: "testName", kind: "unknown", logoUrl: URL(string: "https://res.cloudinary.com/argyle-media/image/upload/v1598543068/partner-logos/amazon.png"))
 
         XCTAssertEqual(mapLinkItem(input), output)
     }
